66327d349b518db4aef7935573ca70b5dbb519f1
Rohit Sahu <rohitsahu@Rohit-MacBook.local>
2026-05-10 12:01:18 +0530

    continue with final keyword

---
+++ 11. OOP/Static_and_Final/Final_Basic.java
@@ -0,0 +1,13 @@
+package Static_and_Final;
+
+public class Final_Basic {
+    static final float PI;
+    static{
+        PI = 3.1425f;  // Declaration of final variable using static block
+    }
+    public static void main(String[] args){
+
+        System.out.println(PI);
+
+    }
+}
