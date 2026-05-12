e8c707a25173255c63b1c793fa1600b0cc48150d
Rohit Sahu <rohitsahu@Rohit-MacBook.local>
2026-05-13 01:23:05 +0530

    learned about packages

---
+++ 12. Packages/Example.java
@@ -0,0 +1,8 @@
+import mypack.Basics;
+
+class Main {
+    public static void main(String[] args) {
+        Basics b = new Basics();
+        b.show();
+    }
+}
---
+++ 12. Packages/mypack/Basics.java
@@ -0,0 +1,7 @@
+package mypack;
+
+public class Basics {
+    public void show() {
+        System.out.println("Hello");
+    }
+}
